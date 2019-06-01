212ecedffbd52b3138559ffd4a948fc14395d45c
俊俊 <382826843@qq.com>
2019-06-01 15:43:49 +0800

    完善依赖管理,增加统一版本号打包插件

--- ucat-boots-common/src/main/java/top/ucat/boots/common/utils/BeanValidator.java
+++ ucat-boots-common/src/main/java/top/ucat/boots/common/utils/BeanValidator.java
@@ -1,6 +1,6 @@
 package top.ucat.boots.common.utils;
 
-import com.sun.deploy.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
